e9b5ccd2979f0b386c519ed8e53f7c7643bb4484
kooky-optimization-7340 <christinafox93@gmail.com>
2019-05-03 22:53:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,13 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+<<<<<<< HEAD
 if board[index] == " " || board[index] == "" || board[index] == nil
   taken = false
 else
   taken = true
+=======
+if board[index] == " "
+  return false
+>>>>>>> ae4a050f0e35b8a9aa7406572d893f92e01ce15b
 end
 end
